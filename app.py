8b87e4e571f813af82bffd6a643a8764a01faca2
qpxdesign <qpxdesign@users.noreply.github.com>
2023-12-25 18:13:29 -0500

    added filtering, timestamps, refined caching system

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ def hello_world():
 
 @app.route("/get-snowy-cities")
 def get_snowy_cities():
+    old_time = time.time()
     def update_snowy_cities():
+        old_time = r.get('last_updated_timestamp')
         r.set('last_updated_timestamp',time.time())
         f = open('./cities.json')
         cities = json.load(f)
@@ -38,14 +40,36 @@ def get_snowy_cities():
         if len(data.keys()) != 0:
             r.set("cached_snow_forecast",json.dumps(data))
         return data
+    
+    def filter_for_snow(data):
+        try:
+            ans = []
+            data = list(data.values())
+            for d in data:
+                print(d)
+                if sum(d['snowfall_hours'][:90]) > 0:
+                    ans.append(d)
+            return ans
+        except Exception as err:
+               print(f"Unexpected {err=}, {type(err)=} when trying to filter cities")
+               return []
+
     last_updated_timestamp = r.get('last_updated_timestamp')
     cached_snow_forecast = r.get("cached_snow_forecast")
     try:
         cached_snow_forecast = json.loads(cached_snow_forecast)
     except:
-        update_snowy_cities()
+        cached_snow_forecast = update_snowy_cities()
+    try:
+        last_updated_timestamp = float(last_updated_timestamp)
+    except:
+        last_updated_timestamp = 0
     if last_updated_timestamp is None or cached_snow_forecast is None or time.time()-float(last_updated_timestamp) > DATA_EXPIRY_LENGTH:
-        update_snowy_cities()
+        cached_snow_forecast = update_snowy_cities()
     else:
         print('using cached weather data')
-        return cached_snow_forecast
+    output = {
+        "timestamp": float(old_time),
+        "cities": filter_for_snow(cached_snow_forecast)
+    }
+    return output
