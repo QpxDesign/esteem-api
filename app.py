88dd72b39c3fa1078f4c114822f6d64b8224645a
qpxdesign <qpxdesign@users.noreply.github.com>
2023-12-25 14:25:16 -0500

    a

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+import redis
+import json 
+import time 
+import requests
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+app = Flask(__name__)
+
+DATA_EXPIRY_LENGTH = 3600 # Seconds, equal to one hour (60*60)
+
+@app.route("/")
+def hello_world():
+    return r.get('foo')
+
+@app.route("/get-snowy-cities")
+def get_snowy_cities():
+    def update_snowy_cities():
+        r.set('last_updated_timestamp',time.time())
+        f = open('./cities.json')
+        cities = json.load(f)
+        data = {}
+        print("trying to update cities")
+        for city in cities:
+            try:
+                request = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={city['latitude']}&longitude={city['longitude']}&hourly=temperature_2m,snowfall&temperature_unit=fahrenheit&precipitation_unit=inch")
+                request = request.json()
+                print(request)
+                data[city['city'] + city['state']] = {
+                    "city": city["city"],
+                    "state":city["state"],
+                    "forecast_hours": request["hourly"]["time"],
+                    "snowfall_hours":request["hourly"]["snowfall"]
+                }
+            except Exception as err:
+               print(f"Unexpected {err=}, {type(err)=} when trying to update city")
+        if len(data.keys()) != 0:
+            r.set("cached_snow_forecast",json.dumps(data))
+        return data
+    last_updated_timestamp = r.get('last_updated_timestamp')
+    cached_snow_forecast = r.get("cached_snow_forecast")
+    try:
+        cached_snow_forecast = json.loads(cached_snow_forecast)
+    except:
+        update_snowy_cities()
+    if last_updated_timestamp is None or cached_snow_forecast is None or time.time()-float(last_updated_timestamp) > DATA_EXPIRY_LENGTH:
+        update_snowy_cities()
+    else:
+        print('using cached weather data')
+        return cached_snow_forecast
